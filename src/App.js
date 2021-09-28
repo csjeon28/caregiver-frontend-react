c14ebb93c1b439ce2bd1cd74afc63438727c49db
Christine Jeon <csjeon28@gmail.com>
2021-09-28 17:04:35 -0400

    added routes to parent & caregiver sign up pages as well as login component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { useEffect } from 'react'
-import autoLogin from './fetches/autoLogin'
+import { Redirect, Switch, Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
+import autoLogin from './fetches/autoLogin'
 import Header from './components/Header'
+import Login from './components/Login'
 import SideDashboard from './components//SideDashboard'
+import CaregiverSignup from './pages/CaregiverSignup'
+import ParentSignup from './pages/ParentSignup'
 
 const App = ({ autoLogin, userData }) => {
   const usertype = localStorage.getItem('usertype')
@@ -15,12 +19,22 @@ const App = ({ autoLogin, userData }) => {
     }
   }, [autoLogin, usertype, usertoken])
 
+  const caregiverHome = (usertype === 'caregiver') ? <Redirect to='/home' /> : <Redirect to='/dashboard' />
+
   return (
     <div className="App">
       {/* <h1>Hello</h1> */}
       {userData.isLoggedIn ? <Header /> : null}
       {userData.isLoggedIn ? <SideDashboard userType={usertype} /> : null}
       <SideDashboard />
+      <div>
+        <Switch>
+          <Route exact path='/' render={() => (userData.isLoggedIn ? caregiverHome : <Redirect to='/login' />)} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/caregiver/signup' component={CaregiverSignup} />
+          <Route exact path='/parent/signup' component={ParentSignup} />
+        </Switch>
+      </div>
     </div>
   )
 }
