ab83294fce41981331f83062bd9977b5d0c23f70
Christine Jeon <csjeon28@gmail.com>
2021-09-27 22:54:00 -0400

    edited app and added store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import './App.css'
+import React, { useEffect } from 'react'
+import autoLogin from './fetches/autoLogin'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import Header from './components/Header'
+import SideDashboard from './components//SideDashboard'
 
-const App = () => {
+const App = ({ autoLogin, userData }) => {
+  const usertype = localStorage.getItem('usertype')
+  const usertoken = localStorage.getItem('token')
+
+  useEffect(() => {
+    if (usertype && usertoken) {
+      autoLogin(usertype)
+    }
+  }, [autoLogin, usertype, usertoken])
+
   return (
     <div className="App">
-
+      {/* <h1>Hello</h1> */}
+      {userData.isLoggedIn ? <Header /> : null}
+      {userData.isLoggedIn ? <SideDashboard userType={usertype} /> : null}
+      <SideDashboard />
     </div>
   )
 }
 
-export default App
+const mapStateToProps = state => ({
+  userData: state.userData,
+})
+
+const mapDispatchToProps = {
+  autoLogin,
+}
+
+App.propTypes = {
+  autoLogin: PropTypes.func.isRequired,
+  userData: PropTypes.instanceOf(Object).isRequired,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
