9f954363152ef5b68bbdf0e0c6fa5567859f9f04
Christine Jeon <csjeon28@gmail.com>
2021-09-29 22:37:03 -0400

    added caregiver and parent job state

---
+++ src/reducers/jobReducer.js
@@ -0,0 +1,26 @@
+const initialState = {
+    jobs: '',
+    loading: false,
+    error: '',
+}
+
+const jobData = (state = initialState, action) => {
+    switch (action.type) {
+        case 'GET_CAREGIVER_JOB_LOADING':
+            return { ...state, loading: true, error: '' }
+        case 'GET_CAREGIVER_JOB':
+            return { ...state, loading: false, jobs: action.payload }
+        case 'GET_CAREGIVER_JOB_FAIL':
+            return { ...state, loading: false, error: action.payload, jobs: '' }
+        case 'GET_PARENT_JOB_LOADING':
+            return { ...state, loading: true, error: '' }
+        case 'GET_PARENT_JOB':
+            return { ...state, loading: false, jobs: action.payload }
+        case 'GET_PARENT_JOB_FAIL':
+            return { ...state, loading: false, error: action.payload, jobs: '' }
+        default:
+            return state
+    }
+}
+
+export default jobData
