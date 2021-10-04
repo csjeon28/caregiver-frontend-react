6ac4535d3fb932fd86dc1104aca5cfe1497e5281
Christine Jeon <csjeon28@gmail.com>
2021-10-04 15:42:40 -0400

    forgot to write headers / fixed issue

--- src/fetches/getAutoLogin.js
+++ src/fetches/getAutoLogin.js
@@ -1,20 +1,22 @@
 import axios from 'axios'
 import * as action from '../actions/userActions'
 
-function autoLogin(userType) {
+function getAutoLogin(userType) {
     return dispatch => {
         dispatch(action.userLoginLoading())
         // change url to api when deployed to heroku
         const api_url = `http://localhost:3001/${userType}/auto_login`
         axios
             .get(api_url, {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-                'Authorization': `Bearer ${localStorage.getItem('token')}`,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`,
+                }
             })
             .then(resp => dispatch(action.userLogin(resp.data, userType)))
             .catch(error => dispatch(action.userLoginFail(error)))
     }
 }
 
-export default autoLogin
+export default getAutoLogin
