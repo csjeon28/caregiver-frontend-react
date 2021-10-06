f335de23fcc044c04f07ca80b9350195ce9c05cd
Christine Jeon <csjeon28@gmail.com>
2021-10-06 18:57:18 -0400

    took out loading

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,20 +1,16 @@
 const initialState = {
     user: '',
     isLoggedIn: false,
-    loading: false,
     error: '',
     userType: '',
 }
 
 const userData = (state = initialState, action) => {
     switch (action.type) {
-        case 'USER_LOGIN_LOADING': {
-            return { ...state, loading: true, isLoggedIn: false }
-        }
+
         case 'USER_LOGIN': {
             return {
                 ...state,
-                loading: false,
                 isLoggedIn: true,
                 user: action.payload,
                 userType: action.userType,
@@ -23,7 +19,6 @@ const userData = (state = initialState, action) => {
         case 'USER_LOGIN_FAIL':
             return {
                 ...state,
-                loading: false,
                 isLoggedIn: false,
                 error: action.payload,
             }
