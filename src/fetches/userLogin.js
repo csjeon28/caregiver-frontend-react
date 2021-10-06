9f085cc8e8251f15ce2b1c0a76151607c60583ff
Christine Jeon <csjeon28@gmail.com>
2021-10-06 18:16:40 -0400

    took out loading and added constants to DRY up code

--- src/fetches/userLogin.js
+++ src/fetches/userLogin.js
@@ -1,23 +1,23 @@
 import axios from 'axios'
 import * as action from '../actions/userActions'
+import { API_ROOT, HEADERS } from '../constants/index'
 
 function userLogin(loginInfo, userType) {
     return dispatch => {
-        dispatch(action.userLoginLoading())
         // change to heroku after dploying
-        const url = `http://localhost:3001/${userType}/login`
+        const url = `${API_ROOT}/${userType}/login`
         axios
             .post(url, loginInfo, {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-                'Authorization': `Bearer ${localStorage.getItem('token')}`,
+                headers: {
+                    HEADERS,
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`,
+                }
             })
             .then(resp => {
                 localStorage.setItem('token', resp.data.token)
                 localStorage.setItem('usertype', userType)
                 dispatch(action.userLogin(resp.data, userType))
             })
-            .then(resp => console.log(resp.data))
             .catch(error => dispatch(action.userLoginFail(error)))
     }
 }
