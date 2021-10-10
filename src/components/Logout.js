ef0db4f6585ca37aaf62918168f0d57f65f45bd8
Christine Jeon <csjeon28@gmail.com>
2021-10-10 18:18:22 -0400

    added mui styling to logout button

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,16 +1,31 @@
 import React, { useState } from 'react'
 import { Redirect } from 'react-router'
+import { useHistory } from 'react-router-dom'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { userLogout } from '../actions/userActions'
+import LogoutIcon from '@mui/icons-material/Logout'
+import { Button, Stack } from '@mui/material'
+import { styled } from '@mui/material/styles'
+import { purple } from '@mui/material/colors'
+
+const ColorButton = styled(Button)(({ theme }) => ({
+    color: theme.palette.getContrastText(purple[500]),
+    backgroundColor: purple[500],
+    '&:hover': {
+        backgroundColor: purple[800],
+    },
+}))
 
 const Logout = ({ userLogout }) => {
+    const history = useHistory()
     const [loggedIn, setLoggedIn] = useState(false)
 
     const handleLogout = e => {
         e.preventDefault()
         setLoggedIn(true)
         userLogout()
+        history.push('/')
     }
 
     if (loggedIn) {
@@ -18,12 +33,16 @@ const Logout = ({ userLogout }) => {
     }
 
     return (
-        <button type='button' onClick={handleLogout}>Log Out</button>
+        <Stack direction='row' spacing={2}>
+            <ColorButton onClick={handleLogout} variant='contained' startIcon={<LogoutIcon />}>
+                Log Out
+            </ColorButton>
+        </Stack>
     )
 }
 
 const mapDispatchToProps = {
-    userLogout,
+    userLogout
 }
 
 Logout.propTypes = {
