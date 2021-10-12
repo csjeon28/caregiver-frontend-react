199e476b1a63ca1637d4e2e5f2bd6a8b41017fff
Christine Jeon <csjeon28@gmail.com>
2021-10-12 15:49:15 -0400

    added get request to fetch all caregivers on parent dashboard

---
+++ src/fetches/getCaregivers.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+import * as action from '../actions/caregiverActions'
+
+function getCaregivers() {
+    return dispatch => {
+        dispatch(action.getCaregiversLoading())
+        const url = 'http://localhost:3001/caregivers'
+        axios
+            .get(url)
+            .then(resp => dispatch(action.getCaregivers(resp.data)))
+            .catch(error => dispatch(action.getCaregiversFail(error)))
+    }
+}
+
+export default getCaregivers
