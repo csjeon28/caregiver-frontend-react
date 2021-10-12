0210ba99d552074c17820eb1436a95748e57fa0f
Christine Jeon <csjeon28@gmail.com>
2021-10-12 15:42:36 -0400

    edited parent card to add/delete profile options

--- src/components/ParentCard.js
+++ src/components/ParentCard.js
@@ -1,38 +1,46 @@
+// import axios from 'axios'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
+// import deleteParent from '../fetches/deleteParent'
 
-const ParentCard = ({ parent }) => (
-    <section>
-        <div>
-            {/* //default image can be added later instead of null */}
-            <img src={parent.profile_image ? parent.profile_image : null} alt={parent.first_name && parent.last_name} />
-        </div>
-        <div>
-            <h3>Name: {parent.first_name}{parent.last_name}</h3>
-            <p>Bio: {parent.bio}</p>
-            <h4>I'm from: {parent.city}, {parent.state}, {parent.country}</h4>
-            <h4>Language(s) Spoken: {parent.language}</h4>
-            <p>
-                Number of Children:
-                {' '}
-                <span>{parent.number_of_children}</span>
-            </p>
-            <p>
-                Hourly Rate:
-                {' '}
-                <span>${parent.hourly_rate}</span>
-                /hour
-            </p>
-        </div>
-        <div>
-            <Link to={`/${parent.id}/match-job`}>Match a Job</Link>
-        </div>
-    </section>
-)
+const ParentCard = ({ parent }) => {
+    // const history = useHistory()
+    // const handleDelete = () => {
+    //     deleteParent(parent.id)
+    //     // const url = `http://localhost:3001/parents/${parent.id}`
+    //     // axios.delete(url)
+    //     history.push('/caregiver-dashboard')
+    // }
+
+    return (
+        <section>
+            <hr />
+            <div>
+                <h3>Parent Name: {parent.first_name} {parent.last_name}</h3>
+                <p>About me: {parent.bio}</p>
+                <h4>I'm from: {parent.city}, {parent.state}, {parent.country}</h4>
+                <h4>Language(s) Spoken: {parent.language}</h4>
+                <p>Number of Children: {parent.number_of_children}</p>
+                <p>
+                    Hourly Rate:
+                    {' '}
+                    <span>${parent.hourly_rate}</span>
+                    /hour
+                </p>
+                <h5>Smoker: {parent.smoker}</h5>
+                <h5>I have pets: {parent.has_pets}</h5>
+            </div>
+            <div>
+                <Link to={`/${parent.id}/match-job`}>Match</Link>&nbsp;&nbsp;
+
+            </div>
+        </section>
+    )
+}
 
 ParentCard.propTypes = {
-    parent: PropTypes.instanceOf(Object).isRequired,
+    parent: PropTypes.instanceOf(Object).isRequired
 }
 
 export default ParentCard
