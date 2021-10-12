71ab27cf7a2bd9d899e219dea9028b3ccc2df65f
Christine Jeon <csjeon28@gmail.com>
2021-10-12 15:37:32 -0400

    added loading option back into schedule reducer

--- src/reducers/scheduleReducer.js
+++ src/reducers/scheduleReducer.js
@@ -6,8 +6,8 @@ const initialState = {
 
 const scheduleData = (state = initialState, action) => {
     switch (action.type) {
-        // case 'GET_SCHEDULE_LOADING':
-        //     return { ...state, loading: true, error: '' }
+        case 'GET_SCHEDULE_LOADING':
+            return { ...state, loading: true, error: '' }
         case 'GET_SCHEDULE':
             return { ...state, loading: false, schedule: action.payload }
         case 'GET_SCHEDULE_FAIL':
