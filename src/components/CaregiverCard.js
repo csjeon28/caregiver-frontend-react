897d02719d68657a4557dca862ed8d9f183c444a
Christine Jeon <csjeon28@gmail.com>
2021-10-17 16:27:56 -0400

    added styling for caregiver card. need to add styling for icons

--- src/components/CaregiverCard.js
+++ src/components/CaregiverCard.js
@@ -1,33 +1,90 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+// import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
+import { styled } from '@mui/material/styles'
+import {
+    Card, CardActions, CardContent, CardHeader, Collapse,
+    Divider, Grid, IconButton, Typography
+} from '@mui/material'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import SmokeFreeIcon from '@mui/icons-material/SmokeFree'
+import DriveEtaIcon from '@mui/icons-material/DriveEta'
+import MedicalServicesIcon from '@mui/icons-material/MedicalServices'
+import { purple, cyan } from '@mui/material/colors'
+
+const ExpandMore = styled((props) => {
+    const { expand, ...other } = props
+    return <IconButton {...other} />
+})(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+    }),
+}))
 
 const CaregiverCard = ({ caregiver }) => {
+    const [expanded, setExpanded] = useState(false)
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded)
+    }
+
+    const renderSmoker = () => {
+        if (caregiver.smoker) return <SmokeFreeIcon />
+    }
+
+    const renderDriver = () => {
+        if (caregiver.ability_to_drive) return <DriveEtaIcon />
+    }
 
     return (
-        <section>
-            <hr />
-            <div>
-                <h3>Caregiver Name: {caregiver.first_name} {caregiver.last_name}</h3>
-                <p>About me: {caregiver.bio}</p>
-                <h4>I'm from: {caregiver.city}, {caregiver.state}</h4>
-                <h4>Date of Birth: {caregiver.date_of_birth}</h4>
-                <h4>Language(s) Spoken: {caregiver.language}</h4>
-                <p>
-                    Hourly Rate:
-                    {' '}
-                    <span>${caregiver.hourly_rate}</span>
-                    /hour
-                </p>
-                <h5>Smoker: {caregiver.smoker}</h5>
-                <h5>I can drive: {caregiver.ability_to_drive}</h5>
-                <h5>Certified: {caregiver.first_aid_cert}{caregiver.CPR_cert}</h5>
-            </div>
-            <div>
-                <Link to={`/${caregiver.id}/match-job`}>Match</Link>&nbsp;&nbsp;
+        <Grid item xs={12} sm={6} md={4} >
+            <Card sx={{ minHeight: 240, height: '100%', display: 'flex', flexDirection: 'column', boxShadow: 5, bgcolor: purple[50], color: cyan[600] }}>
+                <CardHeader
+                    title={caregiver.first_name}
+                    sx={{ textTransform: 'uppercase' }}
+                />
+                <Divider />
+                <CardContent>
+                    <Typography variant='body1' sx={{ color: purple[800] }}>
+                        {caregiver.bio}
+                    </Typography>
+                </CardContent>
+                <CardActions disableSpacing sx={{ marginTop: 'auto' }}>
+                    <IconButton aria-label='add to favorites'>
+                        <FavoriteIcon />
+                    </IconButton>
+                    <Typography variant='body1' color='text.secondary'>
+                        {caregiver.city}, {caregiver.state}
+                    </Typography>
+                    <ExpandMore
+                        expand={expanded}
+                        onClick={handleExpandClick}
+                        aria-expanded={expanded}
+                        aria-label='show more'
+                    >
+                        <ExpandMoreIcon />
+                    </ExpandMore>
+                </CardActions>
+                <Collapse in={expanded} timeout='auto' unmountOnExit>
+                    <CardContent>
+                        <Typography variant='body2' color='text.secondary'>
+                            Language(s) Spoken: &nbsp;{caregiver.language}
+                        </Typography>
+                        <Typography variant='body2' color='text.secondary'>
+                            Hourly Rate: &nbsp;${caregiver.hourly_rate} / hour
+                        </Typography>
+                        {renderSmoker()}
+                        {renderDriver()}
+                    </CardContent>
+                </Collapse>
+                {/* <h5>Certified: {caregiver.first_aid_cert}{caregiver.CPR_cert}</h5> */}
 
-            </div>
-        </section>
+                {/* <Link to={`/${caregiver.id}/match-job`}>Match</Link>&nbsp;&nbsp; */}
+            </Card>
+        </Grid>
     )
 }
 
