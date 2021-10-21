defd4959e1931da89d4b0d456b92c462925a1cd9
Christine Jeon <csjeon28@gmail.com>
2021-10-21 12:08:16 -0400

    fixed headers object by taking out the extra {}

--- src/fetches/userSignup.js
+++ src/fetches/userSignup.js
@@ -3,10 +3,10 @@ import { API_ROOT, HEADERS } from '../constants/index'
 
 function userSignup(loginData, userType) {
     return dispatch => {
-        const url = `${API_ROOT}/${userType}s`;
+        const url = `${API_ROOT}/${userType}s`
         fetch(url, {
             method: 'POST',
-            headers: { HEADERS },
+            headers: HEADERS,
             body: JSON.stringify(loginData)
         })
             .then(resp => resp.json())
