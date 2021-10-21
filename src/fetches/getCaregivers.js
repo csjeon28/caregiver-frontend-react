b6fbd97e1985c6317eeabe6c71b94dee9d367a1c
Christine Jeon <csjeon28@gmail.com>
2021-10-21 12:09:39 -0400

    added API_ROOT constant to url

--- src/fetches/getCaregivers.js
+++ src/fetches/getCaregivers.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 import * as action from '../actions/caregiverActions'
+import { API_ROOT } from '../constants/index'
 
 function getCaregivers() {
     return dispatch => {
-        dispatch(action.getCaregiversLoading())
-        const url = 'http://localhost:3001/caregivers'
+        const url = `${API_ROOT}/caregivers`
         axios
             .get(url)
             .then(resp => dispatch(action.getCaregivers(resp.data)))
