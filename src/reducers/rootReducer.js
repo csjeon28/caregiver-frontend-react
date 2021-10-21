4c780d2e6d61debc1cbfb6287b66c371f161974d
Christine Jeon <csjeon28@gmail.com>
2021-10-21 19:48:42 -0400

    took out schedule reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,14 +2,12 @@ import { combineReducers } from 'redux'
 import userData from './userReducer'
 import parentData from './parentReducer'
 import jobData from './jobReducer'
-import scheduleData from './scheduleReducer'
 import caregiverData from './caregiverReducer'
 
 const rootReducer = combineReducers({
     userData,
     parentData,
     jobData,
-    scheduleData,
     caregiverData
 })
 
