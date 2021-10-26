c8e7f6b1641350899fbfdca29ba5aefbdfe91c81
Christine Jeon <csjeon28@gmail.com>
2021-10-26 16:17:31 -0400

    changed to axios from fetch

--- src/fetches/userSignup.js
+++ src/fetches/userSignup.js
@@ -1,19 +1,18 @@
+import axios from 'axios'
 import * as action from '../actions/userActions'
 import { API_ROOT, HEADERS } from '../constants/index'
 
 function userSignup(loginData, userType) {
     return dispatch => {
         const url = `${API_ROOT}/${userType}s`
-        fetch(url, {
-            method: 'POST',
-            headers: HEADERS,
-            body: JSON.stringify(loginData)
-        })
-            .then(resp => resp.json())
-            .then(data => {
-                localStorage.setItem('token', data.token)
+        axios
+            .post(url, loginData, {
+                headers: HEADERS
+            })
+            .then(resp => {
+                localStorage.setItem('token', resp.data.token)
                 localStorage.setItem('usertype', userType)
-                dispatch(action.userLogin(data))
+                dispatch(action.userLogin(resp.data, userType))
             })
     }
 }
