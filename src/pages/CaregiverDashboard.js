bdeeebf348beba2ecccc14e8c3b40260ea5f8b52
Christine Jeon <csjeon28@gmail.com>
2021-10-26 16:13:45 -0400

    fixed the welcome greeting

--- src/pages/CaregiverDashboard.js
+++ src/pages/CaregiverDashboard.js
@@ -5,6 +5,7 @@ import ParentCard from '../components/ParentCard'
 import getParents from '../fetches/getParents'
 import { Grid, Typography } from '@mui/material'
 import { purple } from '@mui/material/colors'
+import AppDashboard from '../components/AppDashboard'
 
 const CaregiverDashboard = ({ parents, error, getParents, userData }) => {
     const usertoken = localStorage.getItem('token')
@@ -29,11 +30,14 @@ const CaregiverDashboard = ({ parents, error, getParents, userData }) => {
         )
     }
 
+    const welcomeName = userData.user ? userData.user.caregiver.first_name : null
+
     return (
         <>
+            <AppDashboard userData={userData} />
             <Typography variant='h4' component='div'
                 sx={{ ml: 4, color: purple[700], flexGrow: 1, fontFamily: 'Cabin Sketch', fontSize: 28, fontStyle: 'italic', letterSpacing: 3 }}>
-                Welcome back {userData.user.caregiver.first_name}&nbsp;!
+                Welcome back {welcomeName}&nbsp;!
             </Typography>
             <Typography sx={{ mt: 2, ml: 4, mr: 4, mb: -7, fontSize: 20, fontVariantCaps: 'small-caps', bgcolor: purple[700], color: 'white' }}>
                 Browse Parents:
