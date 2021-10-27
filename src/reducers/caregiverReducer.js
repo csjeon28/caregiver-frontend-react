4d80028dc5b11e58331b31bb2b963daab29655d0
Christine Jeon <csjeon28@gmail.com>
2021-10-27 17:43:00 -0400

    took out unnecesarry commented out code

--- src/reducers/caregiverReducer.js
+++ src/reducers/caregiverReducer.js
@@ -9,8 +9,6 @@ const caregiverData = (state = initialState, action) => {
             return { ...state, caregivers: action.payload }
         case 'GET_CAREGIVERS_FAIL':
             return { ...state, error: action.payload, caregivers: '' }
-        // case 'DELETE_CAREGIVER':
-        //     return state.filter(({ id }) => id !== action.payload.id)
         default:
             return state
     }
