551f40cdaf0d615127375b4c7c201e9597fe1616
Christine Jeon <csjeon28@gmail.com>
2021-10-27 18:19:16 -0400

    added post jobs to reducers

--- src/reducers/jobReducer.js
+++ src/reducers/jobReducer.js
@@ -1,23 +1,20 @@
 const initialState = {
     jobs: '',
-    loading: false,
     error: '',
 }
 
 const jobData = (state = initialState, action) => {
     switch (action.type) {
-        // case 'GET_CAREGIVER_JOB_LOADING':
-        //     return { ...state, loading: true, error: '' }
+        case 'POST_JOB':
+            return { ...state, jobs: action.payload }
         case 'GET_CAREGIVER_JOB':
-            return { ...state, loading: false, jobs: action.payload }
+            return { ...state, jobs: action.payload }
         case 'GET_CAREGIVER_JOB_FAIL':
-            return { ...state, loading: false, error: action.payload, jobs: '' }
-        // case 'GET_PARENT_JOB_LOADING':
-        //     return { ...state, loading: true, error: '' }
+            return { ...state, error: action.payload, jobs: '' }
         case 'GET_PARENT_JOB':
-            return { ...state, loading: false, jobs: action.payload }
+            return { ...state, jobs: action.payload }
         case 'GET_PARENT_JOB_FAIL':
-            return { ...state, loading: false, error: action.payload, jobs: '' }
+            return { ...state, error: action.payload, jobs: '' }
         default:
             return state
     }
