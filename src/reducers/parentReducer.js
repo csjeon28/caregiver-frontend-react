6ec147b3b0adecebb2e5ce85314645f7d3cc5b2f
Christine Jeon <csjeon28@gmail.com>
2021-10-27 17:43:22 -0400

    took out unnecessary commented out code

--- src/reducers/parentReducer.js
+++ src/reducers/parentReducer.js
@@ -9,18 +9,6 @@ const parentData = (state = initialState, action) => {
             return { ...state, parents: action.payload }
         case 'GET_PARENTS_FAIL':
             return { ...state, error: action.payload, parents: '' }
-        // case 'UPDATE_PARENT':
-        //     return state.map((s) => {
-        //         if (s.id === action.payload.id) {
-        //             return {
-        //                 ...state, ...action.payload
-        //             }
-        //         } else {
-        //             return s
-        //         }
-        //     })
-        // case 'DELETE_PARENT':
-        //     return state.filter(({ id }) => id !== action.payload.id)
         default:
             return state
     }
