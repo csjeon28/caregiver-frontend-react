f727dcfcc2c300b8aff6ccc72940e76460ec208b
Christine Jeon <csjeon28@gmail.com>
2021-11-02 17:57:01 -0400

    took out caregiver/parent job requests

--- src/reducers/jobReducer.js
+++ src/reducers/jobReducer.js
@@ -6,15 +6,9 @@ const initialState = {
 const jobData = (state = initialState, action) => {
     switch (action.type) {
         case 'POST_JOB':
+            return { jobs: [...state.jobs, action.payload] }
+        case 'GET_JOB_LISTINGS':
             return { ...state, jobs: action.payload }
-        case 'GET_CAREGIVER_JOB':
-            return { ...state, jobs: action.payload }
-        case 'GET_CAREGIVER_JOB_FAIL':
-            return { ...state, error: action.payload, jobs: '' }
-        case 'GET_PARENT_JOB':
-            return { ...state, jobs: action.payload }
-        case 'GET_PARENT_JOB_FAIL':
-            return { ...state, error: action.payload, jobs: '' }
         default:
             return state
     }
