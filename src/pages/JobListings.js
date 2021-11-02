7740b7ed9bc273aa3223096f55bbda48abf662cd
Christine Jeon <csjeon28@gmail.com>
2021-11-02 17:50:24 -0400

    fixed error that wasnt allowing jobs to display

---
+++ src/pages/JobListings.js
@@ -0,0 +1,55 @@
+import { useEffect } from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import getJobListings from '../fetches/getJobListings'
+import AppDashboard from '../components/AppDashboard'
+import JobCard from '../components/JobCard'
+import { Chip, Grid, Typography } from '@mui/material'
+import { purple } from '@mui/material/colors'
+import DriveEtaIcon from '@mui/icons-material/DriveEta'
+
+const JobListings = ({ getJobListings, jobData, userData }) => {
+
+    useEffect(() => {
+        getJobListings(userData.user.parent.id)
+    }, [getJobListings, userData.user.parent.id])
+
+    let jobCard
+
+    if (jobData.jobs) {
+        jobCard = jobData.jobs.map((j, index) => <JobCard key={index} job={j} />)
+    }
+
+    return (
+        <>
+            <AppDashboard userData={userData} />
+            <Grid container spacing={2} sx={{ padding: '2em', mb: -2 }}>
+                <Chip label='Required to Drive' variant='outlined' color='info' icon={<DriveEtaIcon />} />
+            </Grid>
+            <Typography sx={{ mt: 2, ml: 4, mr: 4, mb: -7, fontSize: 20, fontVariantCaps: 'small-caps', bgcolor: purple[700], color: 'white' }}>
+                My Job Listings:
+            </Typography>
+            <Grid container spacing={3} sx={{ padding: '2em' }}>
+                {jobCard}
+            </Grid>
+        </>
+    )
+}
+
+const mapStateToProps = state => ({
+    userData: state.userData,
+    jobData: state.jobData
+})
+
+const mapDispatchToProps = {
+    getJobListings,
+}
+
+JobListings.propTypes = {
+    getJobListings: PropTypes.func.isRequired,
+    userData: PropTypes.instanceOf(Object).isRequired,
+    jobData: PropTypes.instanceOf(Object).isRequired,
+    jobs: PropTypes.oneOfType([PropTypes.string, PropTypes.instanceOf(Array)]).isRequired,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(JobListings)
