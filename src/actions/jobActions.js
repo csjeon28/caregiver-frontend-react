6eda053b03892fa784b1557303ab7866430a7918
Christine Jeon <csjeon28@gmail.com>
2021-11-05 16:27:14 -0400

    added alljobs action

--- src/actions/jobActions.js
+++ src/actions/jobActions.js
@@ -7,3 +7,8 @@ export const getJobListings = data => ({
     type: 'GET_JOB_LISTINGS',
     payload: data
 })
+
+export const getAllJobs = data => ({
+    type: 'GET_ALL_JOBS',
+    payload: data
+})
