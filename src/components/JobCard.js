824c226ddc039c8cd86a07286d4a9b16b06e2f84
Christine Jeon <csjeon28@gmail.com>
2021-11-06 01:20:53 -0400

    added parent name and moved around content in card

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -7,7 +7,7 @@ import {
 } from '@mui/material'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import DriveEtaIcon from '@mui/icons-material/DriveEta'
-import { purple, cyan, blue } from '@mui/material/colors'
+import { pink, purple, cyan, blue } from '@mui/material/colors'
 
 const ExpandMore = styled((props) => {
     const { expand, ...other } = props
@@ -20,7 +20,7 @@ const ExpandMore = styled((props) => {
     }),
 }))
 
-const JobCard = ({ job }) => {
+const JobCard = ({ job, userName, parentData, userData }) => {
     const [expanded, setExpanded] = useState(false)
 
     const handleExpandClick = () => {
@@ -31,6 +31,16 @@ const JobCard = ({ job }) => {
         if (job.required_to_drive) return <DriveEtaIcon sx={{ color: blue[400] }} />
     }
 
+    const parentName = parentData.parents.map(n => {
+        if (n.id === job.parent_id) return n.first_name
+        return null
+    })
+
+    const renderParentName = () => {
+        if (userData.userType === 'caregiver') return parentName
+        if (userData.userType === 'parent') return userName
+    }
+
     return (
         <Grid item xs={12} sm={6} md={4} >
             <Card sx={{ minHeight: 240, height: '100%', display: 'flex', flexDirection: 'column', boxShadow: 5, bgcolor: purple[50], color: cyan[600] }}>
@@ -38,16 +48,16 @@ const JobCard = ({ job }) => {
                     title={job.title}
                     sx={{ textTransform: 'uppercase' }}
                 />
-                <Typography>
-
-                </Typography>
                 <Divider />
+                <Typography variant='body1' align='left' sx={{ boxShadow: 2, padding: 0.7, color: cyan[800], mr: 2, ml: 2, fontSize: 13 }}>
+                    Posted by:
+                    <Typography variant='body2' sx={{ color: pink[200], textTransform: 'uppercase', fontSize: 13, letterSpacing: 1 }}>
+                        {renderParentName()}
+                    </Typography>
+                </Typography>
                 <CardContent>
                     <Typography variant='body1' sx={{ color: purple[800] }}>
                         {job.job_description}
-                    </Typography>
-                    <Typography variant='body2' sx={{ color: purple[800] }}>
-                        Days Needed: {job.specific_days_needed}
                     </Typography>
                 </CardContent>
                 <CardActions disableSpacing sx={{ marginTop: 'auto', ml: 1 }}>
@@ -58,17 +68,16 @@ const JobCard = ({ job }) => {
                         aria-expanded={expanded}
                         aria-label='show more'
                     >
-                        <Typography sx={{ textTransform: 'uppercase', fontSize: 12 }}>View pay rate</Typography>
+                        <Typography sx={{ textTransform: 'uppercase', fontSize: 12 }}>View details</Typography>
                         <ExpandMoreIcon />
                     </ExpandMore>
                 </CardActions>
                 <Collapse in={expanded} timeout='auto' unmountOnExit>
                     <CardContent>
                         <Typography variant='body2' color='text.secondary'>
-                            Hourly Rate: &nbsp;${job.hourly_rate} / hour
-                        </Typography>
-                        <Typography variant='body2' color='text.secondary'>
-                            Number of Children: {job.number_of_children}
+                            Pay Rate: &nbsp;${job.hourly_rate} / hour<br />
+                            Number of Children: {job.number_of_children}<br />
+                            Days Needed: {job.specific_days_needed}
                         </Typography>
                     </CardContent>
                 </Collapse>
@@ -78,7 +87,10 @@ const JobCard = ({ job }) => {
 }
 
 JobCard.propTypes = {
-    job: PropTypes.instanceOf(Object).isRequired
+    job: PropTypes.instanceOf(Object).isRequired,
+    parentData: PropTypes.instanceOf(Object).isRequired,
+    userData: PropTypes.instanceOf(Object),
+    userName: PropTypes.string
 }
 
 export default JobCard
