3cd0dc8c8df5627b7c02fb59085311e02cc10f3c
Christine Jeon <csjeon28@gmail.com>
2021-11-18 15:06:40 -0500

    reducers for post & get job requests

--- src/reducers/jobReducer.js
+++ src/reducers/jobReducer.js
@@ -1,16 +1,25 @@
 const initialState = {
     jobs: '',
-    error: ''
+    requestedJobs: '',
+    error: '',
 }
 
 const jobData = (state = initialState, action) => {
     switch (action.type) {
+        case 'JOB_FAIL':
+            return { ...state, error: action.payload }
         case 'POST_JOB':
             return { jobs: [...state.jobs, action.payload] }
         case 'GET_JOB_LISTINGS':
             return { ...state, jobs: action.payload }
         case 'GET_ALL_JOBS':
             return { ...state, jobs: action.payload }
+        case 'STORE_REQUESTED_JOBS':
+            return { ...state, requestedJobs: action.payload }
+        case 'POST_JOB_REQUEST':
+            return { ...state, requestedJobs: [...state.requestedJobs, { job_id: action.job_id }] }
+        case 'GET_JOB_REQUESTS':
+            return { ...state, requestedJobs: action.payload }
         default:
             return state
     }
