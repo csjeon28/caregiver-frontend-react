eb4ecf05e0c99b0059dd44b9f87d7deec62b3293
Christine Jeon <csjeon28@gmail.com>
2021-12-01 06:14:42 -0500

    got rid of candidatereducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -3,14 +3,12 @@ import userData from './userReducer'
 import parentData from './parentReducer'
 import jobData from './jobReducer'
 import caregiverData from './caregiverReducer'
-import candidateData from './candidateReducer'
 
 const rootReducer = combineReducers({
     userData,
     parentData,
     jobData,
-    caregiverData,
-    candidateData
+    caregiverData
 })
 
 export default rootReducer
