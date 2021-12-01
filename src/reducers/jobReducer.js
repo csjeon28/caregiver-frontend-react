a26bfdd285821fa8f08b3ab7298a6c96f69c5bab
Christine Jeon <csjeon28@gmail.com>
2021-12-01 06:14:03 -0500

    added patch job request

--- src/reducers/jobReducer.js
+++ src/reducers/jobReducer.js
@@ -1,6 +1,6 @@
 const initialState = {
-    jobs: '',
-    requestedJobs: '',
+    jobs: [],
+    requestedJobs: [],
     error: '',
 }
 
@@ -10,6 +10,12 @@ const jobData = (state = initialState, action) => {
             return { ...state, error: action.payload }
         case 'POST_JOB':
             return { jobs: [...state.jobs, action.payload] }
+        case 'PATCH_JOB':
+            // let jobCopy = [...state.jobs.filter(job => job.id !== action.payload)]
+            // return {
+            //     ...state, jobs: [...jobCopy, action.payload]
+            // }
+            return { ...state, jobs: [...state.jobs.find(j => j.id === action.payload)] }
         case 'GET_JOB_LISTINGS':
             return { ...state, jobs: action.payload }
         case 'GET_ALL_JOBS':
@@ -20,6 +26,9 @@ const jobData = (state = initialState, action) => {
             return { ...state, requestedJobs: [...state.requestedJobs, { job_id: action.job_id }] }
         case 'GET_JOB_REQUESTS':
             return { ...state, requestedJobs: action.payload }
+        // case 'REMOVE_CANDIDATE':
+        //     console.log(state.jobs.filter(j => j.id !== action.id))
+        //     return { ...state, requestedJobs: [...state.jobs.filter(job => job.id !== action.job_id)] }
         default:
             return state
     }
