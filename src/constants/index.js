8eb863869401eecf84de109a3da1ff137ad479e2
Christine Jeon <csjeon28@gmail.com>
2021-12-03 01:42:40 -0500

    added frontend url and took out localhost

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,5 @@
 export const API_ROOT = 'https://caregiver-backend-rails.herokuapp.com'
+// export const API_ROOT = 'http://localhost:3001'
 export const HEADERS = {
     'Content-Type': 'application/json',
     Accept: 'application/json',
