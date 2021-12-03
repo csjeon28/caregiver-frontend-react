4dc39a3824b6b4d6ed675cabb25f29d3a45c7938
Christine Jeon <csjeon28@gmail.com>
2021-12-02 23:37:37 -0500

    deploy react portion

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_ROOT = 'http://localhost:3001';
+export const API_ROOT = 'https://caregiver-backend-rails.herokuapp.com'
 export const HEADERS = {
     'Content-Type': 'application/json',
     Accept: 'application/json',
