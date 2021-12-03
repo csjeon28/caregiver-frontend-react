71434d8bd9d847eac8a3019a78f041a21520f5aa
Christine Jeon <csjeon28@gmail.com>
2021-12-03 01:35:56 -0500

    added frontend url for copyright link

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -5,7 +5,7 @@ export default function Copyright(props) {
     return (
         <Typography variant='body2' color='text.secondary' align='center' {...props}>
             {'Copyright © '}
-            <Link color='inherit' href='https://localhost:3000'>
+            <Link color='inherit' href='https://caregiver-frontend-react.herokuapp.com/'>
                 CareGiver App
             </Link>{' '}
             {new Date().getFullYear()}
